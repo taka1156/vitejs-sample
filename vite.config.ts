40dd7f05d4a763d8574bbb77a8ae7d2c2aac0e25
taka <tomo05061@gmail.com>
2021-02-20 22:28:32 +0900

    fix: config修正

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,14 @@ import vue from '@vitejs/plugin-vue';
 // https://github.com/vitejs/vite/tree/main/packages/plugin-vue#example-for-passing-options-to-vuecompiler-dom
 // https://zenn.dev/konnyaku256/scraps/6e8f78642cde2c#comment-d7b575d6d29c05
 export default defineConfig({
+  publicDir: process.env.NODE_ENV === 'production' ? '/vitejs-sample/' : '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, '/src')
     }
+  },
+  build: {
+    assetsDir: './'
   },
   plugins: [vue()]
 });
